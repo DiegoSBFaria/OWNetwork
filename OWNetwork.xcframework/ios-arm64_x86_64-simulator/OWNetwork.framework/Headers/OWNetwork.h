7c0513523dc61d83fd27e44fe47945d84859182b
Diego Faria <diego.sb.faria@gmail.com>
2021-08-27 14:08:45 -0300

    Gera framework

---
+++ OWNetwork.xcframework/ios-arm64_x86_64-simulator/OWNetwork.framework/Headers/OWNetwork.h
@@ -0,0 +1,18 @@
+//
+//  OWNetwork.h
+//  OWNetwork
+//
+//  Created by Diego Faria on 18/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OWNetwork.
+FOUNDATION_EXPORT double OWNetworkVersionNumber;
+
+//! Project version string for OWNetwork.
+FOUNDATION_EXPORT const unsigned char OWNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OWNetwork/PublicHeader.h>
+
+
